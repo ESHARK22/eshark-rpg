264e2d10c014cd13412e56d31c3be0a5311e47bb
Ishaaq Ollite <iollite@hotmail.com>
2023-05-24 21:36:04 +0100

    Bevy starter turtorial

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
 use bevy::app::App;
+use bevy::ecs::component::Component;
+use bevy::ecs::system::{Commands, Query};
+use bevy::ecs::query::With;
+
 
 fn hello_world() {
     println!("Hello World!");
 }
 
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
+
 fn main() {
     App::new()
+        .add_startup_system(add_people)
         .add_system(hello_world)
+        .add_system(greet_people)
         .run();
 }
