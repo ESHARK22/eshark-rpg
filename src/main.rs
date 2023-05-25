b6a0768392c5526b21a3c331c92a127488c0c277
Ishaaq Ollite <iollite@hotmail.com>
2023-05-25 17:07:18 +0100

    bevy spritesheet example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+//! Renders an animated sprite by loading all animation frames from a single image (a sprite sheet)
+//! into a texture atlas, and changing the displayed image periodically.
+
 use bevy::prelude::*;
 
 fn main() {
@@ -42,10 +45,9 @@ fn setup(
     asset_server: Res<AssetServer>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
-    let texture_handle = asset_server.load("assets/RPGMCharacter_v1.0/pngs/up_idle.png");
+    let texture_handle = asset_server.load("textures/rpg/chars/gabe/gabe-idle-run.png");
     let texture_atlas =
         TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), 7, 1, None, None);
-        TextureAtlas::from_grid(texture_handle, Vec2::new(64.0, 64.0), 8, 1, None, None);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
     // Use only the subset of sprites in the sheet that make up the run animation
     let animation_indices = AnimationIndices { first: 1, last: 6 };
@@ -60,3 +62,4 @@ fn setup(
         animation_indices,
         AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
     ));
+}
