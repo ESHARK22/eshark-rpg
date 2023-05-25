3df7d37ba70d76cdd7f60b0f705b2d38b717e060
Ishaaq Ollite <iollite@hotmail.com>
2023-05-25 17:13:04 +0100

    Added proper animations

--- src/main.rs
+++ src/main.rs
@@ -45,9 +45,9 @@ fn setup(
     asset_server: Res<AssetServer>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
-    let texture_handle = asset_server.load("textures/rpg/chars/gabe/gabe-idle-run.png");
+    let texture_handle = asset_server.load("assets/RPGMCharacter_v1.0/pngs/up_idle.png");
     let texture_atlas =
-        TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), 7, 1, None, None);
+        TextureAtlas::from_grid(texture_handle, Vec2::new(64.0, 64.0), 4, 2, None, None);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
     // Use only the subset of sprites in the sheet that make up the run animation
     let animation_indices = AnimationIndices { first: 1, last: 6 };
