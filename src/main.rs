a2902258a673b1644f8e954c5f02f682f4729c93
Ishaaq Ollite <eshark@eshark.tk>
2023-05-25 15:22:54 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -106,6 +106,7 @@ fn setup(
     let texture_handle = asset_server.load("assets/RPGMCharacter_v1.0/pngs/up_idle.png");
     let texture_atlas =
         TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), 7, 1, None, None);
+        TextureAtlas::from_grid(texture_handle, Vec2::new(64.0, 64.0), 8, 1, None, None);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
     // Use only the subset of sprites in the sheet that make up the run animation
     let animation_indices = AnimationIndices { first: 1, last: 6 };
